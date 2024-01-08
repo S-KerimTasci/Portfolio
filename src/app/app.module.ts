aca822b4b2751c9f2815c9052054187708f5a417
Kerim <kerim.tasci@yahoo.de>
2024-01-08 12:40:28 +0100

    about me structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboveTheFoldComponent
+    AboveTheFoldComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
