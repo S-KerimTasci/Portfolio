38ceb4ee089ce77d504971e9cd3343260a1500a4
Kerim <kerim.tasci@yahoo.de>
2024-01-08 09:13:33 +0100

    ATF Background & IAM Text

---
+++ src/app/above-the-fold/above-the-fold.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-above-the-fold',
+  templateUrl: './above-the-fold.component.html',
+  styleUrl: './above-the-fold.component.scss'
+})
+export class AboveTheFoldComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboveTheFoldComponent
   ],
   imports: [
     BrowserModule,
