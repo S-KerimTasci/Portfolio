86c7585cecc815c6e69af36561e7ff1f34439a53
Kerim <kerim.tasci@yahoo.de>
2024-01-09 11:02:16 +0100

    MS icon Div

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
-
+  images = ['1.png', '2.png', '3.png' , '4.png', '5.png','6.png', '7.png', '8.png' , '9.png', '10.png'];
+  titles = ['Angular', 'TypeScript', 'Javascript', 'HTML', 'Firebase', 'GIT', 'CSS', 'Rest-API', 'Scrum', 'Material design'];
 }
