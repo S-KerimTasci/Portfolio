34970aa9f9237914c3cf5f1ec6e6b4438700339c
Kerim <kerim.tasci@yahoo.de>
2024-01-18 11:10:25 +0100

    portfolio elements added

--- src/app/portfolio-element/portfolio-element.component.ts
+++ src/app/portfolio-element/portfolio-element.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,6 +10,15 @@ export class PortfolioElementComponent {
 
   constructor(private router: Router) { }
 
+  @Input() fd: string = '';
+  @Input() imgURL: string = '';
+  @Input() imgALT: string = '';
+  @Input() title: string = '';
+  @Input() skills: string = '';
+  @Input() description: string = '';
+  @Input() livelink: string = '';
+  @Input() githublink: string = '';
+  
 
   visitPage(i: string) {
     const absoluteUrl = i.startsWith('http://') || i.startsWith('https://') ? i : `http://${i}`;
