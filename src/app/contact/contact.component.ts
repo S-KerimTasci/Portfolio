f35ac28a414087f188824d7603fc887a33019af4
Kerim <kerim.tasci@yahoo.de>
2024-01-21 15:25:38 +0100

    add spinner and reset form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -10,6 +10,8 @@ export class ContactComponent implements AfterViewInit {
   @ViewChild('emailField') emailField!: ElementRef;
   @ViewChild('messageField') messageField!: ElementRef;
   @ViewChild('sendButton') sendButton!: ElementRef;
+  @ViewChild('formField') formField!: ElementRef;
+
 
   nameFieldVar: any;
   emailFieldVar: any;
@@ -18,52 +20,57 @@ export class ContactComponent implements AfterViewInit {
 
 
   ngAfterViewInit(): void {
-  this.nameFieldVar = this.nameField.nativeElement;
-  this.emailFieldVar = this.emailField.nativeElement;
-  this.messageFieldVar = this.messageField.nativeElement;
-  this.sendButtonVar = this.sendButton.nativeElement
+    this.nameFieldVar = this.nameField.nativeElement;
+    this.emailFieldVar = this.emailField.nativeElement;
+    this.messageFieldVar = this.messageField.nativeElement;
+    this.sendButtonVar = this.sendButton.nativeElement
   }
 
 
   async sendMail() {
-
-    //"https://@w01eb728.kasserver.com/send_mail/send_mail.php"
-
     this.disableForm();
-
-    console.log('sending Mail')
-
-    // loading spinner
 
     let fd = new FormData();
     fd.append('name', this.nameFieldVar.value);
-    //fd.append('email', this.emailFieldVar.value)
-    fd.append('message', this.messageFieldVar.value + ' ++++email: '+this.emailFieldVar.value) ;
+    fd.append('message', this.messageFieldVar.value + ' ++++email: ' + this.emailFieldVar.value);
     await fetch('https://kerim-tasci.de/send_mail/send_mail.php', {
-      method : 'POST',
+      method: 'POST',
       body: fd
     }
     )
+
     // Text anzeigen Nachticht gesendet 
     this.enableForm();
   }
+
 
   disableForm() {
     this.nameFieldVar.disabled = true;
     this.emailFieldVar.disabled = true;
     this.messageFieldVar.disabled = true;
     this.sendButtonVar.disabled = true;
+    document.getElementById('spinner')?.classList.remove('d-none');
   }
 
-  enableForm(){
+
+  enableForm() {
     this.nameFieldVar.disabled = false;
     this.emailFieldVar.disabled = false;
     this.messageFieldVar.disabled = false;
     this.sendButtonVar.disabled = false;
+    document.getElementById('spinner')?.classList.add('d-none');
+    this.resetForm();
   }
 
 
-  scrollTop(){
+  resetForm(){
+    this.nameFieldVar.value = '';
+    this.emailFieldVar.value = '';
+    this.messageFieldVar.value = '';
+  }
+
+
+  scrollTop() {
     document.getElementById('ATF')?.scrollIntoView();
   }
 }
