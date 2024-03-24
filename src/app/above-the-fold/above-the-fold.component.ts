5ad89f27fb6d2926c18b5edb2bfd0f46d14dc909
Kerim <kerim.tasci@yahoo.de>
2024-03-24 16:55:03 +0100

    aos

--- src/app/above-the-fold/above-the-fold.component.ts
+++ src/app/above-the-fold/above-the-fold.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { VisitPageService } from '../visit-page.service';
 import { SharedDataService } from '../shared-data-headerHeight.service';
 import { Router } from '@angular/router';
+import AOS from "aos";
 
 @Component({
   selector: 'app-above-the-fold',
   templateUrl: './above-the-fold.component.html',
   styleUrl: './above-the-fold.component.scss'
 })
-export class AboveTheFoldComponent{
+export class AboveTheFoldComponent implements OnInit{
 
   constructor(public visitPageService: VisitPageService, public sharedDataService: SharedDataService, private router: Router) { }
+
+  ngOnInit(): void {
+    AOS.init();
+  }
+
 }
