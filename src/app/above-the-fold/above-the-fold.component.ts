5e592d8aa3910d1e0ab5cb1b2665e47a07e4a3de
Kerim <kerim.tasci@yahoo.de>
2024-03-24 22:37:27 +0100

    aos bug fix

--- src/app/above-the-fold/above-the-fold.component.ts
+++ src/app/above-the-fold/above-the-fold.component.ts
@@ -2,19 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { VisitPageService } from '../visit-page.service';
 import { SharedDataService } from '../shared-data-headerHeight.service';
 import { Router } from '@angular/router';
-import AOS from "aos";
 
 @Component({
   selector: 'app-above-the-fold',
   templateUrl: './above-the-fold.component.html',
   styleUrl: './above-the-fold.component.scss'
 })
-export class AboveTheFoldComponent implements OnInit{
+export class AboveTheFoldComponent {
 
   constructor(public visitPageService: VisitPageService, public sharedDataService: SharedDataService, private router: Router) { }
-
-  ngOnInit(): void {
-    AOS.init();
-  }
 
 }
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslationService } from '../translation.service';
+import AOS from "aos";
+import 'aos/dist/aos.css'
+import 'aos/dist/aos.js'
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit {
 
   constructor(public translationService: TranslationService) { }
 
+
+  ngOnInit() {
+    AOS.init();
+  }
 
 }
