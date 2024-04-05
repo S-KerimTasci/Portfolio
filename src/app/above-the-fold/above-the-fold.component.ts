0ad26c9a7c5298f0261038baee1e5e462f474f1c
Kerim <kerim.tasci@yahoo.de>
2024-04-05 18:40:08 +0200

    ATF 100vh

--- src/app/above-the-fold/above-the-fold.component.ts
+++ src/app/above-the-fold/above-the-fold.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { VisitPageService } from '../visit-page.service';
 import { SharedDataService } from '../shared-data-headerHeight.service';
 import { Router } from '@angular/router';
@@ -10,6 +10,12 @@ import { Router } from '@angular/router';
 })
 export class AboveTheFoldComponent {
 
-  constructor(public visitPageService: VisitPageService, public sharedDataService: SharedDataService, private router: Router) { }
+  constructor(public visitPageService: VisitPageService, public sharedDataService: SharedDataService, private router: Router) {}
 
+  desktopWith:number = window.innerWidth;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.desktopWith = event.target.innerWidth;
+  }
 }
